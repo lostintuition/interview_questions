9a00160dd7f286073840a0fc1615f06e0222cde1
lostintuition <theanonymous@gmail.com>
2015-05-17 15:40:17 -0700

    Add the initial questions

---
+++ 8/12.rb
@@ -0,0 +1,9 @@
+#! /usr/bin/env ruby
+
+# Let L be a singly linked list. Assume its nodes are numbered starting at 0.
+# Define the even-odd merge of L to be the list consisting of the even-numbered nodes follow by the odd-numbered nodes.
+# The even-dd merge is illustrated in Figure 8.10
+#
+# Write a function that computes the even-odd merge
+
+puts "hi"
