529415aafbb56b4a6d32cb22df52f3ab26f0cadf
lostintuition <garrettsato@Garretts-MacBook-Pro.local>
2015-05-17 21:39:19 -0700

    Start 14.rb

---
+++ 8/14.rb
@@ -0,0 +1,11 @@
+#! /usr/bin/env ruby
+
+# A postings list is a singly linked list with an additional "jump" field
+# The "jump" field points to any other node
+#
+# Implement a function that takes a postings list and returns a copy of it.
+# You can modify the original list but must restore it to its original state before returning
+
+def copy_postings_list(list)
+  list->  
+end
