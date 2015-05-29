21b6e0a9ba4c686f5067fac5c9c805d7f069c0ac
lostintuition <lostintuition>
2015-05-28 21:36:10 -0700

    Finished more problems. Added a linked list implementation

---
+++ 10/9.rb
@@ -0,0 +1,5 @@
+#! /usr/bin/env ruby
+
+# The direct implementation of an inorder traversal using recusion has O(h) space complexity, where h is the hight of the tree. Recursion can be removed with an explicit stack, but the space complexity remains O(h)
+#
+# Write a nonrecursive program for performing inorder traversal on a binary tree. Assume nodes have parent fields.
