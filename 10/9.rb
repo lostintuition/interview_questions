bc012976b1a6c4e88e2e98e0140dca1b33b88be7
lostintuition <theanonymous@gmail.com>
2015-05-31 21:55:30 -0700

    Initial binary tree work

--- 10/9.rb
+++ 10/9.rb
@@ -1,5 +1,34 @@
 #! /usr/bin/env ruby
 
+require_relative "binary_tree"
+
 # The direct implementation of an inorder traversal using recusion has O(h) space complexity, where h is the hight of the tree. Recursion can be removed with an explicit stack, but the space complexity remains O(h)
 #
 # Write a nonrecursive program for performing inorder traversal on a binary tree. Assume nodes have parent fields.
+
+def in_order_stack(root)
+  stack = Array.new
+  stack.push(root)
+  node = root
+
+  while (!stack.empty?)
+    node = stack.last
+    while (!node.left.nil?)
+      puts "hi"
+      stack.push(node.left)
+      next
+    end
+    stack.pop
+    puts node
+    if (!node.right.nil?)
+      stack.push(node.right)
+    end
+  end
+end
+
+b = BinaryTree.new
+b.set_up_example_tree
+
+root = b.root
+
+in_order_stack(root)
