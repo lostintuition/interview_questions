fce6760e3a6ac363178e62f26c16ce36454b5416
lostintuition <lostintuition>
2015-06-21 17:27:29 -0700

    Completed binary tree problems

--- 10/9.rb
+++ 10/9.rb
@@ -2,33 +2,13 @@
 
 require_relative "binary_tree"
 
-# The direct implementation of an inorder traversal using recusion has O(h) space complexity, where h is the hight of the tree. Recursion can be removed with an explicit stack, but the space complexity remains O(h)
+# The successor of a node in a binary tree is the node that appears immediately after the given node in an inorder traversal.
 #
-# Write a nonrecursive program for performing inorder traversal on a binary tree. Assume nodes have parent fields.
+# Design an algorithm that computes the successor of a node in a binary tree. Assume that each node stores its parent
 
-def in_order_stack(root)
-  stack = Array.new
-  stack.push(root)
-  node = root
-
-  while (!stack.empty?)
-    node = stack.last
-    while (!node.left.nil?)
-      puts "hi"
-      stack.push(node.left)
-      next
-    end
-    stack.pop
-    puts node
-    if (!node.right.nil?)
-      stack.push(node.right)
-    end
-  end
+def inorder_successor(node)
+  
 end
 
 b = BinaryTree.new
 b.set_up_example_tree
-
-root = b.root
-
-in_order_stack(root)
